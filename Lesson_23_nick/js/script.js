ffa0389e3ed06afea8f567744d394ea8c9dd15de
NickWeisz <107553792+NickWeisz@users.noreply.github.com>
2022-12-08 18:26:17 -0700

    Add files via upload

---
+++ Lesson_23_nick/js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// for (let i = 1; i<=5 ; i++){
+//   console.log(i)
+// }
+
+// let num = 0;
+// while(num < 3){
+//   console.log(`Number ${num}`);
+//   num++;
+// }
+
+// Firstfor: for (let num = 0; num < 2; num++){
+//   for(let size = 0; size < 3; size++){
+//     console.log(size)
+//     if(size == 1){
+//       break Firstfor
+//     }
+//   }
+// }
+
+// let num = 1.005 + Number.EPSILON;
+// let newNum = Math.round(num * 100) / 100;
+// console.log(newNum);
+
+// let value = "135.58px";
+// value = parseFloat(value);
+// console.log(value);
+
+// let value = 58 + "frilancer";
+// if (isNaN(value)) {
+//   console.log("результат выражения NaN");
+// }
+
+// let a = "фрилансер";
+// console.log(a.includes("н") ? a[a.indexOf("н")] : null);
+
+// let a = "фрилансер";
+// console.log(a.toUpperCase());
+
+// let a = "фрилансер";
+// console.log(a.slice(3, 6));
