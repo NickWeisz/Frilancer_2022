6bd71de56f7c33938372ea84162e52b84bb2c77c
NickWeisz <107553792+NickWeisz@users.noreply.github.com>
2022-12-13 21:40:58 -0700

    Add files via upload

---
+++ Lesson_24_nick/js/script.js
@@ -0,0 +1,37 @@
+"use strict";
+
+// let arr = ["Ваня", "Иштван"];
+// arr.push("Оля");
+// arr[1] = "Петя";
+// console.log(arr.splice(0, 1));
+// arr.unshift("Маша", "Паша");
+// console.log(arr);
+
+// let arr = ["Ваня", "Иштван", "Оля"];
+// let a = arr.splice(1, 1);
+// console.log(arr);
+// console.log(a);
+
+// const getData = document.querySelector("div");
+// console.log(getData.getAttribute("data-say-hi"));
+
+// const elements = document.querySelector("ul");
+// const yonchiElement = elements.lastElementChild.innerHTML;
+// console.log(yonchiElement);
+
+// const allLikeClass = document.querySelectorAll(".like");
+// console.log(allLikeClass);
+
+// const delay = setTimeout(setScrollTo, 1000);
+
+// function setScrollTo() {
+//   window.scrollTo({
+//     top: 100,
+//     left: 0,
+//     behavior: "smooth",
+//   });
+// }
+
+// const item = document.querySelector("h1");
+// const getItemCoords = item.getBoundingClientRect();
+// console.log(getItemCoords);
